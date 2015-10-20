9d6a17121939ab0810a78741767a18870bad7517
Maxim Bondarchuk <maksim.bondarchuk@gmail.com>
2015-10-21 00:52:45 +0300

    Bugs fixed

--- T7_Computer_Systems_Lab1/Divided.cs
+++ T7_Computer_Systems_Lab1/Divided.cs
@@ -10,10 +10,17 @@ namespace T7_Computer_Systems_Lab1
     class Divided
     {
         List<Thread> units = new List<Thread>();
+        List<List<int>> mA;
 
-        public Divided()
+        public Divided(List<List<int>> mA, int units_number)
+        {
+            mA = new List<List<int>>();
+        }
+
+        public void unit_work(int my_idx)
         {
 
         }
+
     }
 }
