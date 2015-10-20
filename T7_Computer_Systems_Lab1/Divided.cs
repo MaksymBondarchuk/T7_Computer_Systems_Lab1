aa47adfcdd78a0f41fc6b4ba0939262007a4d37c
Maxim Bondarchuk <maksim.bondarchuk@gmail.com>
2015-10-21 00:22:17 +0300

    Divided class added

---
+++ T7_Computer_Systems_Lab1/Divided.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace T7_Computer_Systems_Lab1
+{
+    class Divided
+    {
+        List<Thread> units = new List<Thread>();
+
+        public Divided()
+        {
+
+        }
+    }
+}
