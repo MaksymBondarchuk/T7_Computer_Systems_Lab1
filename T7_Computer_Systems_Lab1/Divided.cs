cb45512fe8b27dc49644626deb4272eec621155c
Maxim Bondarchuk <maksim.bondarchuk@gmail.com>
2015-10-21 01:49:02 +0300

    Divided class

--- T7_Computer_Systems_Lab1/Divided.cs
+++ T7_Computer_Systems_Lab1/Divided.cs
@@ -11,16 +11,78 @@ namespace T7_Computer_Systems_Lab1
     {
         List<Thread> units = new List<Thread>();
         List<List<int>> mA;
+        List<List<int>> mC = new List<List<int>>();
+        int tact_length = 200;
+
+        int tact_nmb = -1;
+
+        List<int> free_rows = new List<int>();
+
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+        Mutex mtx = new Mutex();
+
+
 
         public Divided(List<List<int>> mA, int units_number)
         {
-            mA = new List<List<int>>();
+            for (int i = 0; i < mA[0].Count; i++)
+            {
+                mC.Add(new List<int>());
+                for (int j = 0; j < mA.Count; j++)
+                    mC[i].Add(0);
+            }
+
+            for (int i = 0; i < mA.Count; i++)
+                free_rows.Add(i);
+
+            for (int i = 0; i < units_number; i++)
+            {
+                units.Add(new Thread(() => unit_work(i)));
+                units[i].Start();
+            }
+
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(tact_length);
+            dispatcherTimer.Start();
         }
 
-        public void unit_work(int my_idx)
+        public async void unit_work(int my_idx)
         {
+            int my_tact_nmb = -1;
 
+            while (true)
+            {
+                while (my_tact_nmb < tact_nmb)
+                {
+                    mtx.WaitOne();
+
+                    if (free_rows.Count == 0)
+                        return;
+
+                    int work_with_row = free_rows[0];
+
+                    for (int i = 0; i < mA[0].Count; i++)
+                        mC[i][free_rows[0]] = mA[free_rows[0]][i];
+
+                    mtx.ReleaseMutex();
+                    my_tact_nmb++;
+
+                    await Task.Delay(tact_length / 5);
+                }
+            }
         }
 
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (0 < free_rows.Count)
+                tact_nmb++;
+            else
+            {
+                dispatcherTimer.Stop();
+                for (int i = 0; i < units.Count; i++)
+                    units[i].Abort();
+            }
+        }
     }
 }
