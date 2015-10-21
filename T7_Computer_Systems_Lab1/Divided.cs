cddaaae888836b635a3f250d38da4fd82e92e99a
Maxim Bondarchuk <maksim.bondarchuk@gmail.com>
2015-10-21 14:30:28 +0300

    Update 1

--- T7_Computer_Systems_Lab1/Divided.cs
+++ T7_Computer_Systems_Lab1/Divided.cs
@@ -20,8 +20,10 @@ namespace T7_Computer_Systems_Lab1
 
         System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
 
-        Mutex mtx = new Mutex();
+        static Mutex mtx = new Mutex();
+        bool flag_mutex = true;
 
+        System.IO.StreamWriter logfile = new System.IO.StreamWriter("log.log");
 
         // Will transpose mA
         public Divided(List<List<int>> mA, int units_number)
@@ -36,37 +38,50 @@ namespace T7_Computer_Systems_Lab1
             for (int i = 0; i < mA.Count; i++)
                 free_rows.Add(i);
 
+            this.mA = mA;
+
             for (int i = 0; i < units_number; i++)
-                units.Add(new Thread(() => unit_work(i)));
+            {
+                Thread t = new Thread(new ThreadStart(unit_work));
+                t.Name = i.ToString();
+                units.Add(t);
+                //units.Add(new Thread(() => unit_work(i)));
+            }
 
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             dispatcherTimer.Interval = new TimeSpan(tact_length);
-            
+
+            logfile.WriteLine("All created\n");
         }
 
-        public async void unit_work(int my_idx)
+        public void unit_work()
         {
             int my_tact_nmb = -1;
+            Console.WriteLine(Thread.CurrentThread.Name + " started");
 
             while (true)
             {
-                while (my_tact_nmb < tact_nmb)
+                if (my_tact_nmb < tact_nmb)
                 {
                     mtx.WaitOne();
+                    logfile.WriteLine(Thread.CurrentThread.Name + " alive");
+
 
                     if (free_rows.Count == 0)
                         return;
 
                     int work_with_row = free_rows[0];
 
+                    logfile.WriteLine(Thread.CurrentThread.Name + " taked " + work_with_row.ToString());
+
                     for (int i = 0; i < mA[0].Count; i++)
                         mC[i][free_rows[0]] = mA[free_rows[0]][i];
 
                     mtx.ReleaseMutex();
                     my_tact_nmb++;
-
-                    await Task.Delay(tact_length / 5);
                 }
+
+                Thread.Sleep(tact_length / 5);
             }
         }
 
@@ -90,6 +105,7 @@ namespace T7_Computer_Systems_Lab1
             dispatcherTimer.Start();
 
             units[0].Join();
+            logfile.Close();
             return mC;
         }
     }
