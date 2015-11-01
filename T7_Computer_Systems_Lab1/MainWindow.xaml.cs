e9cf27c2404317637d00a349c28037a80fac6281
Dimitrij Kozlov <ikzdimitrij@gmail.com>
2015-11-01 14:44:37 +0200

    New form design

--- T7_Computer_Systems_Lab1/MainWindow.xaml.cs
+++ T7_Computer_Systems_Lab1/MainWindow.xaml.cs
@@ -54,24 +54,36 @@ namespace T7_Computer_Systems_Lab1
         {
             var s = ((Convert.ToInt32(lCollMx1.Content.ToString()) + 1) % 10).ToString();
             lCollMx1.Content = (s == "0")?  "1" : s;
+            if (lType.Content == "Addition")
+                lCollMx2.Content = lCollMx1.Content;
         }
 
         private void lColl_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             var s = (Convert.ToInt32(lCollMx1.Content.ToString()) - 1).ToString();
             lCollMx1.Content = (s == "0") ? "9" : s;
+            if (lType.Content == "Addition")
+                lCollMx2.Content = lCollMx1.Content;
         }
 
         private void lRow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             var s = ((Convert.ToInt32(lRowMx1.Content.ToString()) + 1) % 10).ToString();
             lRowMx1.Content = (s == "0") ? "1" : s;
+            if (lType.Content != "Addition")
+                lCollMx2.Content = lRowMx1.Content;
+            else
+                lRowMx2.Content = lRowMx1.Content;
         }
 
         private void lRow_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             var s = (Convert.ToInt32(lRowMx1.Content.ToString()) - 1).ToString();
             lRowMx1.Content = (s == "0") ? "9" : s;
+            if (lType.Content != "Addition")
+                lCollMx2.Content = lRowMx1.Content;
+            else
+                lRowMx2.Content = lRowMx1.Content;
         }
 
         private void lNumberOfProc_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -126,6 +138,73 @@ namespace T7_Computer_Systems_Lab1
 
 
             print_Matrix(_matrix, tbMx1);
+        }
+
+        private void bTsp_Click(object sender, RoutedEventArgs e)
+        {
+            tbMx2.Visibility = Visibility.Hidden;
+
+            gbMx2.Visibility = Visibility.Hidden;
+            lCollMx2.Visibility = Visibility.Hidden;
+            lSignOfMultMx2.Visibility = Visibility.Hidden;
+            lRowMx2.Visibility = Visibility.Hidden;
+
+            lType.Content = "Transposition";
+            
+        }
+
+        private void bAdd_Click(object sender, RoutedEventArgs e)
+        {
+            tbMx2.Visibility = Visibility.Visible;
+
+            gbMx2.Visibility = Visibility.Hidden;
+            lCollMx2.Visibility = Visibility.Hidden;
+            lSignOfMultMx2.Visibility = Visibility.Hidden;
+            lRowMx2.Visibility = Visibility.Hidden;
+
+            lType.Content = "Addition";
+
+            lCollMx2.Content = lCollMx1.Content;
+            lRowMx2.Content = lRowMx1.Content;
+        }
+
+        private void bMlt_Click(object sender, RoutedEventArgs e)
+        {
+
+            tbMx2.Visibility = Visibility.Visible;
+
+            gbMx2.Visibility = Visibility.Visible;
+            lCollMx2.Visibility = Visibility.Visible;
+            lSignOfMultMx2.Visibility = Visibility.Visible;
+            lRowMx2.Visibility = Visibility.Visible;
+
+            lType.Content = "Multiplication";
+
+            lCollMx2.Content = lRowMx1.Content;
+        }
+
+        private void lRowMx2_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var s = ((Convert.ToInt32(lRowMx2.Content.ToString()) + 1) % 10).ToString();
+            lRowMx2.Content = (s == "0") ? "1" : s;
+        }
+
+        private void lRowMx2_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var s = (Convert.ToInt32(lRowMx2.Content.ToString()) - 1).ToString();
+            lRowMx2.Content = (s == "0") ? "9" : s;
+        }
+
+        private void tbMx2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var coll = Convert.ToInt32(lCollMx2.Content);
+            var row = Convert.ToInt32(lRowMx2.Content);
+            _unit = Convert.ToInt32(lNumberOfProc.Content);
+
+            var rmg = new RandomMatrixGenerator();
+            _matrix = rmg.Generate(row, coll);
+
+            print_Matrix(_matrix, tbMx2);
         }
     }
 }
